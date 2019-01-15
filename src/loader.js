6fe0d29ec612fa513d7eb1819d5f5031c9f522d3
Max <crazymax101@gmail.com>
2019-01-15 08:59:56 +0700

    feat(Loader): Extract name from filepath
    Loade has access to processing file. Loader now tries to match filepath against /[^//\@].vue/].
     If
    it matched, it extract name and adds it to meta
    
    #9

--- src/loader.js
+++ src/loader.js
@@ -3,6 +3,14 @@ const extractMeta = require('./extractMeta.js');
 module.exports = function loader(code) {
   const meta = extractMeta(code);
 
+  const re = /[^\\/@]+\.vue/;
+  const matched = this.resourcePath.match(re);
+  const name = matched ? matched[0].replace('.vue', '') : '';
+
+  if (name) {
+    meta.name = name;
+  }
+
   // TODO: make via AST
   const modifiedCode = code.replace(
     'export default {',
