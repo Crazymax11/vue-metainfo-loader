ed87e1dac7b0715bf482693a46629e1770567423
Константин Дорощенко <indiefrontgeek@ya.ru>
2019-02-20 15:29:33 +0700

    Add events table component

--- storybook/Components/EventsTable.vue
+++ storybook/Components/EventsTable.vue
@@ -1,28 +1,107 @@
 <script>
+import Type from './Type.vue';
+import BaseTable from './BaseTable.vue';
+
+/**
+ * @typedef {{
+ *    name: string,
+ *    description: string,
+ *    payload: string
+ * }} Event
+ */
+
+/**
+ * @typedef {{
+ *      name: string,
+ *      description: string,
+ *      type: string
+ * }} CustomType
+ */
+
+/**
+ * Events Table
+ */
 export default {
+  components: {
+    Type,
+    BaseTable,
+  },
+
   props: {
+    /**
+     * Список пропс
+     *
+     * @type {Event[]}
+     */
     events: {
       type: Array,
+      required: true,
+    },
+
+    /**
+     * Список кастомных типов, которые могут находиться внутри
+     * переданных типов пропс
+     *
+     * @type {CustomType[]}
+     */
+    customTypes: {
+      type: Array,
+      default() {
+        return [];
+      },
     },
   },
-  computed: {
-    // debug
-    str() {
-      return JSON.stringify(this.events);
-    },
+
+  data() {
+    return {
+      columns: [
+        {
+          id: 'name',
+          title: 'Name',
+        },
+        {
+          id: 'payload',
+          title: 'Payload',
+        },
+      ],
+    };
   },
 };
 </script>
 
 <template>
-  <div>
-    <div>events</div>
-    <div>{{ str }}</div>
-  </div>
+  <BaseTable :columns="columns" :data="events">
+    <template slot="cell" slot-scope="{ column, dataItem }">
+      <template v-if="column.id === 'name'">
+        <div class="name">
+          <span class="name-title">{{ dataItem.name }}</span>
+          <span class="name-description">{{ dataItem.description }}</span>
+        </div>
+      </template>
+
+      <template v-else-if="column.id === 'payload' && dataItem.payload">
+        <Type
+          class="type"
+          :type="dataItem.payload"
+          :custom-types="customTypes"
+        ></Type>
+      </template>
+    </template>
+  </BaseTable>
 </template>
 
 <style lang="scss" scoped>
-.test {
-  color: red;
+@import '../styles/common';
+
+.name {
+  &-title {
+    display: block;
+    margin-bottom: 6px;
+  }
+  &-description {
+    display: block;
+    color: $color-gray50;
+    font-size: 14px;
+  }
 }
 </style>
